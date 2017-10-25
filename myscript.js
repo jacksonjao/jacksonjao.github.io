df549a94bc4f1d4ce38f7dc21287d04dcf7812bb
Jhon <albeosorioz88x@gmail.com>
2017-10-25 15:02:15 -0500

    .

--- myscript.js
+++ myscript.js
@@ -260,68 +260,64 @@ for (var i = 0; i < cantidadDeSecciones; i++) {
 
 //inicia con opacidad y animación del logo
 
-$("#home").animate({
-    opacity: 1,
 
-}, 500, function () {
+setTimeout(
+    function () {
+        $(".logo-uno").css({
+            'padding': '0',
+            'opacity': '1'
+        })
 
-    setTimeout(
-        function () {
-            $(".logo-uno").css({
-                'padding': '0',
-                'opacity': '1'
-            })
-
-        }, 0);
+    }, 0);
 
 
 
-    setTimeout(
-        function () {
-            $(".logo-tres").css({
-                'transition': '1s',
-                'transform': 'translate(0em,0em) rotate(-0deg)',
-                'opacity': '1',
-                'background-position': 'left'
-            });
+setTimeout(
+    function () {
+        $(".logo-tres").css({
+            'transition': '1s',
+            'transform': 'translate(0em,0em) rotate(-0deg)',
+            'opacity': '1',
+            'background-position': 'left'
+        });
 
-        }, 1000);
-
-
-    setTimeout(
-        function () {
-            $(".logo-dos").css({
-                'transform': 'translateX(0)',
-                'opacity': '1'
-            });
-
-        }, 2000);
-
-    setTimeout(
-        function () {
-            $(".logo-cuatro").css({
-                'transform': 'translateX(0)',
-                'opacity': '1'
-            });
-        }, 3000);
-
-    setTimeout(
-        function () {
-
-            showText(".introduccion", intro, 0, 60);
-        }, 4000);
-
-    setTimeout(
-        function () {
-            $(".rect-see-my-work").css({
-                'opacity': '1'
-            });
+    }, 1000);
 
 
-        }, 8000);
+setTimeout(
+    function () {
+        $(".logo-dos").css({
+            'transform': 'translateX(0)',
+            'opacity': '1'
+        });
+
+    }, 2000);
+
+setTimeout(
+    function () {
+        $(".logo-cuatro").css({
+            'transform': 'translateX(0)',
+            'opacity': '1'
+        });
+    }, 3000);
+
+setTimeout(
+    function () {
+
+        showText(".introduccion", intro, 0, 60);
+    }, 4000);
+
+setTimeout(
+    function () {
+        $(".rect-see-my-work").css({
+            'opacity': '1'
+        });
 
 
-});
+    }, 8000);
+
+
+
 
 
 //metodo de texto letra por letra
@@ -344,7 +340,7 @@ var showText = function (target, message, index, interval) {
 var skillActivado = [false, false, false];
 
 
-    
+
 $(".skill-titulo").each(function (i) {
 
     $(this).click(function () {
@@ -439,8 +435,8 @@ $(".skill-titulo").each(function (i) {
 
 
 
-var skillsResponsive= function(ancho){
-    
+var skillsResponsive = function (ancho) {
+
     if (ancho <= 992) {
         $(".skill-titulo").each(function (i) {
             $(this).click(function () {
@@ -477,37 +473,127 @@ var skillsResponsive= function(ancho){
 }
 
 
-  skillsResponsive(ancho);
+skillsResponsive(ancho);
 
 
 
 //section my work
 
 
-var covers = ["url(img/cover-app-hed.jpg)", "url(img/cover-app-tonelist.png)", "url(img/cover-desktop-pixel.png)", "url(img/cover-app-topotapp.png)"]
 
-var numX=2;
-var numY=2;
-var anchoTrabajos=ancho;
-var altoTrabajos=$("#my-work").height();
+var covers = ["url(img/cover-app-hed.jpg)", "url(img/cover-app-tonelist.png)", "url(img/cover-desktop-pixel.png)", "url(img/cover-app-topotapp.png)", "url(img/cover-app-topotapp.png)"]
+
+
+var trabajos = [["https://mir-s3-cdn-cf.behance.net/project_modules/1400/63959756457739.59af8a5896928.jpg","https://mir-s3-cdn-cf.behance.net/project_modules/1400/c0b75056457739.59af8a589643d.gif","https://mir-s3-cdn-cf.behance.net/project_modules/1400/2d9f3e56457739.59af8a58960cf.jpg","https://mir-s3-cdn-cf.behance.net/project_modules/1400/90028956457739.59af8a5897439.jpg","https://mir-s3-cdn-cf.behance.net/project_modules/1400/7f7e9e56457739.59af8fbe24927.jpg"],["https://mir-s3-cdn-cf.behance.net/project_modules/1400/c0a09958026619.59ed471fe089b.png","https://mir-s3-cdn-cf.behance.net/project_modules/1400/27407c58026619.59ed471fe048d.png","https://mir-s3-cdn-cf.behance.net/project_modules/1400/7bbf6658026619.59ed471fdff7c.png","https://mir-s3-cdn-cf.behance.net/project_modules/1400/c4634658026619.59ed471fdfaab.png"],["https://mir-s3-cdn-cf.behance.net/project_modules/1400/d633f158026119.59ed40a013bf7.png","https://mir-s3-cdn-cf.behance.net/project_modules/1400/61cd1558026119.59ed40a01384f.png","https://mir-s3-cdn-cf.behance.net/project_modules/1400/4bb3ba58026119.59ed40a014675.png","https://mir-s3-cdn-cf.behance.net/project_modules/1400/62f65d58026119.59ed40a0140fc.png"],["https://mir-s3-cdn-cf.behance.net/project_modules/1400/7d2d4756357879.59ed28d2d2f25.jpg","https://mir-s3-cdn-cf.behance.net/project_modules/1400/4a41c056357879.59ed28d2d366f.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c2c32456357879.59ed3d4346d7e.gif"]
+               ]
+
+for (var i = 0; i < covers.length; i++) {
+    $(".section-mywork").append(" <div  class='trabajo' ><div class = 'cuadro' ></div></div>");
+}
+
+
+
+
+
+
+
+var numX = 2;
+var numY = 2;
+var anchoTrabajos = ancho;
+var altoTrabajos = $("#my-work").height();
+
+
+$($(".trabajo")[0]).mouseover(function () {
+    $($(".cuadro")[0]).css({
+        "opacity": "0.8"
+    });
+});
+
 
 
 $(".trabajo").each(function (i) {
+
     $(this).css({
         'background-image': covers[i],
-        'width':anchoTrabajos/numX,
-        'height':altoTrabajos/numY
+        'width': anchoTrabajos / numX,
+        'height': altoTrabajos / numY
     });
-    
+
+    $(this).mouseover(function () {
+
+        $($(".cuadro")[i]).css({
+            "opacity": "0.5"
+        });
+    });
+
+
+    $($(".cuadro")[i]).mouseout(function () {
+        $(".cuadro").css({
+            "opacity": "0"
+        });
+    })
+
+
+    $(this).click(function () {
+
+        for(var j=0;j<trabajos[i].length;j++){
+        $(".contenido-trabajo").append("<img src="+trabajos[i][j]+">");
+        }
+        
+        
+        $(".contenido-trabajo").css({
+            'height': '100%'
+        })
+
+        $("body").css({
+            'overflow': 'hidden'
+        })
+
+        setTimeout(
+            function () {
+                $(".icon-close").css({
+                    'height': '50px'
+                })
+            }, 500)
+
+
+    });
+
+
+
 });
 
-var trabajoResponsive= function(ancho,alto,numX,numY){
-    $(".trabajo").each(function (i) {
+
+
+$(".icon-close").click(function () {
+    $(".contenido-trabajo").css({
+        'height': '0'
+    })
     $(this).css({
-        'width':ancho/numX,
-        'height':alto/numY
-    });
+        'height': '0'
+    })
+
+
+
+    $("body").css({
+        'overflow': 'visible'
+    })
+
+    setTimeout(
+        function () {
+            $(".contenido-trabajo").empty();
+        }, 500)
 });
+
+
+
+var trabajoResponsive = function (ancho, alto, numX, numY) {
+    $(".trabajo").each(function (i) {
+        $(this).css({
+            'width': ancho / numX,
+            'height': alto / numY
+        });
+    });
 }
 
 
@@ -539,23 +625,24 @@ $(".home-icon-down>a>i").mouseout(function () {
 });
 
 
-if(ancho<=600){
-    alto=$("#my-work").height();
-    trabajoResponsive(ancho,alto,1,4);   
+if (ancho <= 600) {
+    alto = $("#my-work").height();
+    trabajoResponsive(ancho, alto, 1, 4);
 }
 
 
 //aquí van los que necesitan ser redimensionados
 $(window).resize(function () {
     ancho = $(window).width();
-    alto=$("#my-work").height();
+    alto = $("#my-work").height();
     skillsResponsive(ancho);
-   
-    if(ancho>600){
-        trabajoResponsive(ancho,alto,2,2);} 
-    else{
-       trabajoResponsive(ancho,alto,1,4);
+
+    if (ancho > 600) {
+        trabajoResponsive(ancho, alto, numX, numY);
+    } else {
+
+        trabajoResponsive(ancho, alto, 1, 4);
     }
 
-    
+
 });
