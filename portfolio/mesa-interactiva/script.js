83f30c69daf28079fb561007188b0cbb6dae5280
Jhon <albeosorioz88x@gmail.com>
2017-11-08 20:02:03 -0500

    .

--- portfolio/mesa-interactiva/script.js
+++ portfolio/mesa-interactiva/script.js
@@ -16,27 +16,9 @@
    }(document, 'script', 'facebook-jssdk'));
 
 
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-    
-    
-});
 
 
-{
-    status: 'connected',
-    authResponse: {
-        accessToken: '...',
-        expiresIn:'...',
-        signedRequest:'...',
-        userID:'...'
-    }
-}
 
-<fb:login-button 
-  scope="public_profile,email"
-  onlogin="checkLoginState();">
-</fb:login-button>
 
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
