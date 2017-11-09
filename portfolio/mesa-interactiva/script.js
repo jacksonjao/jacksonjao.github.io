762a5927e311e297e07b094e9c7e249ec5bf7af3
Jhon <albeosorioz88x@gmail.com>
2017-11-08 19:44:37 -0500

    se creó el proyecto - mesa interactiva

---
+++ portfolio/mesa-interactiva/script.js
@@ -0,0 +1,29 @@
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : 'your-app-id',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.11'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+FB.api(
+    "/{user-id}/music",
+    function (response) {
+        if (response && !response.error) {
+            /* handle the result */
+            alert("holi")
+        } else {
+            alert("holi")
+
+        }
+    }
+);
