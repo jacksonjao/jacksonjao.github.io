63edadcb687ca8a6c758da10f96c5f9adc87d75d
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2018-04-01 20:32:36 -0500

    Se subieron 2 trabajos

--- myscript.js
+++ myscript.js
@@ -562,36 +562,90 @@ skillsResponsive(ancho);
 
 
 
-var covers = ["url(img/cover-app-hed.jpg)", "url(img/cover-app-tonelist.png)", "url(img/cover-desktop-pixel.png)", "url(img/cover-app-topotapp.png)"]
+var covers = [
+    "url(img/cover-app-hed.jpg)",
+    "url(img/cover-app-eule.png)",
+    "url(img/cover-app-tonelist.png)",
+    "url(img/cover-app-who.png)",
+    "url(img/cover-desktop-pixel.png)",
+    "url(img/cover-app-topotapp.png)"]
 
-var backgrounds = ["img/pattern.png", "img/pattern.png", "img/pattern.png", "img/pattern-topotapp.jpg"];
+var backgrounds = [
+    "img/pattern.png",
+    "img/pattern.png",
+    "img/pattern.png",
+    "img/pattern.png",
+    "img/pattern.png",
+    "img/pattern-topotapp.jpg"
+];
 
-var trabajos = [["https://mir-s3-cdn-cf.behance.net/project_modules/1400/63959756457739.59af8a5896928.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/c0b75056457739.59af8a589643d.gif", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/2d9f3e56457739.59af8a58960cf.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/90028956457739.59af8a5897439.jpg"], ["https://mir-s3-cdn-cf.behance.net/project_modules/1400/c0a09958026619.59ed471fe089b.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/27407c58026619.59ed471fe048d.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/7bbf6658026619.59ed471fdff7c.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/c4634658026619.59ed471fdfaab.png"], ["https://mir-s3-cdn-cf.behance.net/project_modules/1400/d633f158026119.59ed40a013bf7.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/61cd1558026119.59ed40a01384f.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/4bb3ba58026119.59ed40a014675.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/62f65d58026119.59ed40a0140fc.png"], ["https://mir-s3-cdn-cf.behance.net/project_modules/1400/7d2d4756357879.59ed28d2d2f25.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/4a41c056357879.59ed28d2d366f.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c2c32456357879.59ed3d4346d7e.gif"]
-               ]
-
-
-var titulos = ["HED", "Tonelist", "Pixel eater", "Topotapp"];
-
-var descripciones = ["HED es la aplicación oficial del evento Hoy es Diseño 2017-1, con es esta aplicación se pueden ver con detalle los talleres, conferencias del evento con su respectivo ponente, fecha y lugar, también puedes poner una foto el marco oficial del evento, ver el mapa del lugar o inscribirse. En esta versión de Hoy es Diseño también tuve la oportunidad de desarrollar ", "Tonelist is an Android Application that improves the DJ - Costumer communication in an Event. It’s easy! The DJ can create an event and people can access to it with a unique code, The DJ will add sogns to a list and you vote for the songs you want to be played. The DJ will know what songs are being the most voted.", "Pixel eater is a simple java aplication made in the designing with algorithms class. It uses processing library to create a graphic interface and manipulate the pixels in the image. Little automaton creatures start moving across the screen and eating the pixels in one side of the screen and those pixels start apearing on the other side of the screen.", "TopoTapp is a mini video game made for the network programming class, this mini game uses a desktop screen controlled by Android device, I programmed this video game on java using Processing library and Android SDK, Android device gets connected to the server through UDP protocol. TopoTapp was inspired in a game for a fair event and the Pokémon character Diglett."];
-
-var tags = [["html", "css","javascript"],["android","firebase","illustrator","java"],["java"],["java","android","photoshop","illustrator"]];
-
-
-
-
-var links = [["https://www.behance.net/gallery/56457739/Aplicacion-evento-Hoy-Es-Diseno","https://play.google.com/store/apps/details?id=it.save.hed","portfolio/hoy-es-diseno/index.html"],["https://github.com/jacksonjao/ToneList","https://www.behance.net/gallery/58026619/Tonelist","https://play.google.com/store/apps/details?id=it.save.tonelist"],["https://github.com/jacksonjao/PixelEater","https://www.behance.net/gallery/58026119/Pixel-Eater"],["https://github.com/jacksonjao/TopoTapp_server-game","https://www.behance.net/gallery/56357879/Topotapp"]];
-
-
-var paginas=[["behance","playstore","internet"],["github","behance","playstore"],["github","behance"],["github","behance"]]
+var trabajos = [
+    ["https://mir-s3-cdn-cf.behance.net/project_modules/1400/63959756457739.59af8a5896928.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/c0b75056457739.59af8a589643d.gif", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/2d9f3e56457739.59af8a58960cf.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/90028956457739.59af8a5897439.jpg"],
+    ["https://mir-s3-cdn-cf.behance.net/project_modules/1400/61992b58434185.59fbf39cd228a.png"],
+    ["https://mir-s3-cdn-cf.behance.net/project_modules/1400/c0a09958026619.59ed471fe089b.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/27407c58026619.59ed471fe048d.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/7bbf6658026619.59ed471fdff7c.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/c4634658026619.59ed471fdfaab.png"],
+    ["https://mir-cdn.behance.net/v1/rendition/project_modules/1400/b8bf2d63922377.5ac1840555d27.png"],
+    ["https://mir-s3-cdn-cf.behance.net/project_modules/1400/d633f158026119.59ed40a013bf7.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/61cd1558026119.59ed40a01384f.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/4bb3ba58026119.59ed40a014675.png", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/62f65d58026119.59ed40a0140fc.png"],
+    ["https://mir-s3-cdn-cf.behance.net/project_modules/1400/7d2d4756357879.59ed28d2d2f25.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/1400/4a41c056357879.59ed28d2d366f.jpg", "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c2c32456357879.59ed3d4346d7e.gif"]
+]
 
 
+var titulos = [
+    "HED",
+    "Eule",
+    "Tonelist",
+    "Who",
+    "Pixel eater",
+    "Topotapp"
+];
+
+var descripciones = [
+    "HED es la aplicación oficial del evento Hoy es Diseño 2017-1, con es esta aplicación se pueden ver con detalle los talleres, conferencias del evento con su respectivo ponente, fecha y lugar, también puedes poner una foto el marco oficial del evento, ver el mapa del lugar o inscribirse. En esta versión de Hoy es Diseño también tuve la oportunidad de desarrollar ",
+    "Eule es una aplicación pensada en y para los estudiantes de Universidades o Colegios que estén dispuestos a donar materiales de estudio que ya no necesitan, también se puede solicitar esas donaciones por la misma aplicación.",
+    "Tonelist is an Android Application that improves the DJ - Costumer communication in an Event. It’s easy! The DJ can create an event and people can access to it with a unique code, The DJ will add sogns to a list and you vote for the songs you want to be played. The DJ will know what songs are being the most voted.",
+    "Who es una Aplicación o videojuego programado en Android que conecta múltiples jugadores a través de un servidor TCP programado en Java, el juego consiste en destapar cartas pares, el último que termine en destaparlas perderá y tendrá que pagar la apuesta o cualquier cosa acordada.",
+    "Pixel eater is a simple java aplication made in the designing with algorithms class. It uses processing library to create a graphic interface and manipulate the pixels in the image. Little automaton creatures start moving across the screen and eating the pixels in one side of the screen and those pixels start apearing on the other side of the screen.",
+    "TopoTapp is a mini video game made for the network programming class, this mini game uses a desktop screen controlled by Android device, I programmed this video game on java using Processing library and Android SDK, Android device gets connected to the server through UDP protocol. TopoTapp was inspired in a game for a fair event and the Pokémon character Diglett."
+];
+
+var tags = [
+    ["html", "css","javascript","java","android"],
+    ["java","android","illustrator","firebase"],
+    ["java","android","illustrator","firebase"],
+    ["java","android","illustrator","photoshop"],
+    ["java"],
+    ["java","android","photoshop","illustrator"]
+];
 
 
 
-var numX = 2;
+
+var links = [
+    ["https://www.behance.net/gallery/56457739/Aplicacion-evento-Hoy-Es-Diseno","https://play.google.com/store/apps/details?id=it.save.hed","portfolio/hoy-es-diseno/index.html"],
+    ["https://www.behance.net/gallery/58434185/Eule","https://drive.google.com/open?id=1uqBHagi1HYAfjKg80pjwv8WfrclnbVny"],
+    ["https://github.com/jacksonjao/ToneList","https://www.behance.net/gallery/58026619/Tonelist","https://play.google.com/store/apps/details?id=it.save.tonelist"],
+    ["https://www.behance.net/gallery/63922377/WHO","https://github.com/jacksonjao/Who-server"],
+    ["https://github.com/jacksonjao/PixelEater","https://www.behance.net/gallery/58026119/Pixel-Eater"],
+    ["https://github.com/jacksonjao/TopoTapp_server-game","https://www.behance.net/gallery/56357879/Topotapp"]
+];
+
+
+var paginas=[
+    ["behance","playstore","internet"],
+    ["behance","playstore"],
+    ["github","behance","playstore"],
+    ["behance","playstore"],
+    ["github","behance"],
+    ["github","behance"]
+]
+
+
+
+
+
+var numX = 3;
 var numY = 2;
-var numXCel = 1;
-var numYCel = 4
+var numXCel = 2;
+var numYCel = 3;
 var anchoTrabajos = ancho;
 var altoTrabajos = $("#my-work").height();
 
