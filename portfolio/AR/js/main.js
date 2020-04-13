3a0bed1f444786e179cb69bad298b39813c3a657
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2020-04-12 20:04:25 -0500

    no message

--- portfolio/AR/js/main.js
+++ portfolio/AR/js/main.js
@@ -1,6 +1,4 @@
 new AnimationPng('assets/woman/bird/', 'birds','birds-animation' , 14);
-new AnimationPng('assets/woman/background/', 'bg','bg-animation' , 2);
-
-new AnimationPng('assets/woman/woman/', 'girl','woman-animation' , 14);
+//new AnimationPng('assets/woman/woman/', 'girl','woman-animation' , 14);
 
 
