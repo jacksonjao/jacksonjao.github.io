8ddf07b65a963c5bd9ef31f4759da9bbc9365443
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2020-04-16 18:29:49 -0500

    no message

--- portfolio/AR/js/main.js
+++ portfolio/AR/js/main.js
@@ -1,7 +1,14 @@
 new AnimationPng('assets/woman/bird/', 'birds','birds-animation' , 15);
 new AnimationPng('assets/woman/woman/', 'girl','woman-animation' , 14);
 
-function range(self, targe, axis) {
-    const element = document.getElementById(targe);
-    element.getAttribute('position')[axis] =self.value
+function range(self, target, attribute, key) {
+    const element = document.getElementById(target);
+    if(key) {
+        element.getAttribute(attribute)[key] = self.value
+        console.log(attribute + ' ' + key + ': '+element.getAttribute(attribute)[key])
+
+    } else {
+        element.setAttribute(attribute, self.value);
+        console.log(attribute + ': '+element.getAttribute(attribute))
+    }
 }
