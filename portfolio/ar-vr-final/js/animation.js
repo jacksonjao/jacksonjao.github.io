3b9d8110634ade3e426059c256375b72d0a13405
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2020-04-24 20:05:13 -0500

    no message

---
+++ portfolio/ar-vr-final/js/animation.js
@@ -0,0 +1,38 @@
+// La estructura de los archivos debe de ser la siguiente 'nombre_#.png'
+class AnimationPng {
+    constructor(sequences, mainNameSequences, id, numberOfFiles) {
+        AFRAME.registerComponent(id, {
+            init: function() {
+                // load the .pngs
+                let loader = new THREE.TextureLoader()
+                this.pngArray = []
+
+                for (let i = 0; i<numberOfFiles; i++) {
+                    this.pngArray.push(loader.load(sequences+mainNameSequences+'_'+i+'.png'));
+                }
+
+                this.el.addEventListener('loaded', e => {
+                    let mesh = this.el.getObject3D("mesh");
+                    this.material = mesh.material
+
+                    var i = 0
+                    this.id = setInterval(e => {
+                        if (i >= this.pngArray.length) i = 0;
+                        this.material.map = this.pngArray[i++]
+                        this.material.needsUpdate = true
+                    }, 60)
+                })
+            },
+            remove: function() {
+                clearInterval(this.id);
+                // free the memory
+                for (let i = 0; i < this.pngArray.length; i++) {
+                    this.pngArray[i].dispose();
+                }
+            }
+        })
+    }
+}
+
+
+
---
+++ portfolio/ar-vr-final/js/main.js
@@ -0,0 +1,53 @@
+new AnimationPng('assets/sprites/art1/bird/', 'birds','birds-animation' , 15);
+new AnimationPng('assets/sprites/art1/woman/', 'girl','woman-animation' , 14);
+
+function range(self, target, attribute, key) {
+    const element = document.getElementById(target);
+
+
+    if(key) {
+        element.getAttribute(attribute)[key] = self.value;
+        getLableFromInput(self).innerHTML = `${attribute} ${key}: ${self.value} `;
+        console.log(attribute + ' ' + key + ': '+element.getAttribute(attribute)[key])
+
+    } else {
+        element.setAttribute(attribute, self.value);
+        getLableFromInput(self).innerHTML = `${attribute}: ${self.value} `;
+        console.log(attribute + ': '+element.getAttribute(attribute))
+    }
+}
+
+
+function getLableFromInput(input) {
+
+    var labels = document.getElementsByTagName('label');
+    for (var i = 0; i < labels.length; i++) {
+        if(labels[i].htmlFor === input.id){
+            return labels[i];
+        }
+    }
+}
+
+
+window.ondevicemotion = function(event) {
+    document.getElementById('motion').innerText=event.toString()
+    console.log(event);
+}
+
+function handleMotionEvent(event) {
+
+
+    var x = event.accelerationIncludingGravity.x;
+    var y = event.accelerationIncludingGravity.y;
+    var z = event.accelerationIncludingGravity.z;
+    document.getElementById('device_motion_X').innerText="X: "+ x;
+    document.getElementById('device_motion_Y').innerText="Y: "+ y;
+    document.getElementById('device_motion_Z').innerText="Z: "+ z;
+    document.getElementById('camera').getAttribute('position').y=y;
+
+    // Do something awesome.
+}
+
+
+window.addEventListener("devicemotion", handleMotionEvent, true);
+
