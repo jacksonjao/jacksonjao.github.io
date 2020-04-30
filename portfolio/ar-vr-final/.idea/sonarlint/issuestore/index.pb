93425220e71883cb848db9583b530145eb0b79ec
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2020-04-30 08:48:09 -0500

    no message

---
+++ portfolio/ar-vr-final/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+?
+js/animation.js,2/c/2cfed5b1949677287195d359955ef5e51746951c
+9
+	style.css,4/6/46e3381e9569c09e85a30b9e5a2af7525ee9fd78
+:
+
+js/main.js,d/a/dae13fd6f331d1c91ab58b06e81d05cb8958bfb1
+:
+
+index.html,f/6/f6013a00b362253c64368d6eebc50ea2131754e2
--- portfolio/ar-vr-final/js/main.js
+++ portfolio/ar-vr-final/js/main.js
@@ -1,9 +1,61 @@
 new AnimationPng('assets/sprites/art1/bird/', 'birds','birds-animation' , 15);
 new AnimationPng('assets/sprites/art1/woman/', 'girl','woman-animation' , 14);
 
-function range(self, target, attribute, key) {
-    const element = document.getElementById(target);
+/*
+let elementSelected ="birds";
+function initSliders() {
+const inputs = document.getElementsByTagName("input");
+const labels = document.getElementsByTagName('label');
+    for (let i =0; i< inputs.length; i++) {
+        const input = inputs[i];
 
+      //  const targetElement = document.getElementById(tP.id);
+        const outerHtml =input.outerHTML.split('(')[1].split([')'])[0].split(',');
+        try {
+            input.id = outerHtml[1].split("'")[1]+"_"+outerHtml[2].split("'")[1]+"_"+outerHtml[3].split("'")[1];
+        } catch (e) {
+            input.id = outerHtml[1].split("'")[1]+"_"+outerHtml[2].split("'")[1];
+
+        }
+        const tP = {
+            id: input.id.split('_')[0],
+            attribute: input.id.split('_')[1],
+            property: input.id.split('_')[2]?input.id.split('_')[2]:''
+        }
+       // input.value = targetElement.getAttribute(tP.attribute)[tP.property];
+        labels[i].htmlFor = input.id;
+        labels[i].innerHTML = `${tP.attribute} ${tP.property}: ${input.value} `
+    }
+
+    const elements = document.getElementsByClassName('item');
+
+    for (let i =0; i< elements.length; i++) {
+        const node = document.createElement("li");
+        const textnode = document.createTextNode(elements[i].id);
+        node.onclick = function(){
+            elementSelected = elements[i].id;
+            document.getElementById('titleSelected').innerText = elementSelected;
+        };
+        node.appendChild(textnode);
+        document.getElementById("elementList").appendChild(node);
+    }
+
+}
+
+setTimeout(()=>{
+    initSliders();
+
+},3000);
+
+function range(self, target, attribute, key) {
+    let element;
+    if (target === 'select'){
+       element = document.getElementById(elementSelected);
+
+    } else {
+       element = document.getElementById(target);
+
+    }
 
     if(key) {
         element.getAttribute(attribute)[key] = self.value;
@@ -28,26 +80,16 @@ function getLableFromInput(input) {
     }
 }
 
-
-window.ondevicemotion = function(event) {
-    document.getElementById('motion').innerText=event.toString()
-    console.log(event);
+let menu = false;
+function openMenu() {
+    menu = !menu;
+    const element = document.getElementById('menu');
+    if(menu){
+        element.classList.add("open");
+    } else {
+        element.classList.remove("open");
+    }
 }
 
-function handleMotionEvent(event) {
-
-
-    var x = event.accelerationIncludingGravity.x;
-    var y = event.accelerationIncludingGravity.y;
-    var z = event.accelerationIncludingGravity.z;
-    document.getElementById('device_motion_X').innerText="X: "+ x;
-    document.getElementById('device_motion_Y').innerText="Y: "+ y;
-    document.getElementById('device_motion_Z').innerText="Z: "+ z;
-    document.getElementById('camera').getAttribute('position').y=y;
-
-    // Do something awesome.
-}
-
-
-window.addEventListener("devicemotion", handleMotionEvent, true);
+*/
 
