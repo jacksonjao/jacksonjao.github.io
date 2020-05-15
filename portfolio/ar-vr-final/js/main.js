e206e2c6cffa993a4ed5e3564d4ff2666f1f3585
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2020-05-15 03:14:59 -0500

    no message

--- portfolio/ar-vr-final/js/main.js
+++ portfolio/ar-vr-final/js/main.js
@@ -1,5 +1,4 @@
 new AnimationPng('assets/sprites/art1/bird/', 'birds','birds-animation' , 150);
-new AnimationPng('assets/sprites/art1/background/', 'bg','bg-animation' , 15);
 new AnimationPng('assets/sprites/art1/woman/', 'girl','woman-animation' , 14);
 
 /*
