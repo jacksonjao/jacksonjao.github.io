a752c8bc256b6dc7be63410f098be4da6128a1ba
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2020-05-20 21:57:30 -0500

    no message

--- portfolio/ar-vr-final/js/main.js
+++ portfolio/ar-vr-final/js/main.js
@@ -1,5 +1,22 @@
+
 new AnimationPngBySrc('assets/sprites/art1/birds_animation_group/', 'bird','birds-animation' , 40);
 new AnimationPngBySrc('assets/sprites/art1/woman/', 'girl','woman-animation' , 13);
+
+function openFullscreen() {
+    const elem = document.body
+
+    console.log(elem)
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+
 
 /*
 let frameRate = 0;
