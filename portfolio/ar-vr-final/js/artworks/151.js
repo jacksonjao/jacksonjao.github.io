f34c712027534e2410f50d3af6db483d4d9c90aa
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2020-07-21 18:13:11 -0500

    no message

---
+++ portfolio/ar-vr-final/js/artworks/151.js
@@ -0,0 +1,1 @@
+new AnimationPngBySrc('assets/artworks/151/america/', 'america','america-animation' , 90);
---
+++ portfolio/ar-vr-final/js/artworks/172.js
@@ -0,0 +1,1 @@
+new AnimationPngBySrc('assets/artworks/172/happy/', 'happy','happy-animation' , 299);
