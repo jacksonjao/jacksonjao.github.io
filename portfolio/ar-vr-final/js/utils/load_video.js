e6b39f271f5c83ca301f88a8f0198019701e0a0f
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2020-08-21 00:27:27 -0500

    no message

--- portfolio/ar-vr-final/js/utils/load_video.js
+++ portfolio/ar-vr-final/js/utils/load_video.js
@@ -1,3 +1,16 @@
-let video = document.getElementById('video');
-video.play();
+let counter = 0;
+THREE.DefaultLoadingManager.onLoad = function ( ) {
+    counter++;
+    if(counter===2){
+        let videos = document.getElementsByTagName('video');
+        let loadingElement = document.getElementById('loading');
+        loadingElement.style.visibility = 'hidden';
+        loadingElement.style.opacity = 0;
+        for(let i = 0; i<videos.length; i++){
+            const video = videos[i];
+            video.play();
+        }
+    }
+
+};
 
