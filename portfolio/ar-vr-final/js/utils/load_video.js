73e74af9dbc6831d5cebfdb661b57356c842bf11
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2020-08-20 23:48:00 -0500

    no message

--- portfolio/ar-vr-final/js/utils/load_video.js
+++ portfolio/ar-vr-final/js/utils/load_video.js
@@ -1,29 +1,3 @@
-
-AFRAME.registerComponent('video', {
-            init: function() {
-                this.el.addEventListener('loaded', element => {
-                    document.querySelector('a-assets').fileLoader.load(
-                        element.target.getAttribute('src'),
-                        function ( data ) {
-                            let miStorage = window.localStorage;
-                            let isPageLoaded = !!miStorage.getItem('isPageLoaded');
-                            if(!isPageLoaded) {
-                                location.reload();
-                                miStorage.setItem('isPageLoaded', 'true');
-                            }else {
-                                miStorage.clear();
-                                let loadingElement = document.getElementById('loading');
-                                loadingElement.style.visibility = 'hidden';
-                                loadingElement.style.opacity = 0;
-                            }
-                        },
-                    );
-
-                })
-            },
-});
-
-
-
-
+let video = document.getElementById('video');
+video.play();
 
