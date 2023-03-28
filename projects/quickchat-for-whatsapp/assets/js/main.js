2fe23739fc83c3185df0c49172be96bc15b33aaa
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2023-03-27 20:07:22 -0500

    no message

--- projects/quickchat-for-whatsapp/assets/js/main.js
+++ projects/quickchat-for-whatsapp/assets/js/main.js
@@ -1,12 +1,45 @@
-import { openWhatsapp, inputValidator, paste, main } from '/assets/js/logic.js';
-
-
-main();
 const inputElementRef =  document.getElementById('jao-ws__input')
 const sendButtonElementRef =  document.getElementById('jao-ws__send-button')
 const sendButtonWrapperElementRef =  document.getElementById('jao-ws__send-button-wrapper')
 const errorMessageElementRef =  document.getElementById('jao-ws__error-message')
 const pasteButtonElementRef =  document.getElementById('jao-ws__paste-button')
+
+
+function heightScreenFixer() {
+    const appHeight = () => {
+        const doc = document.documentElement
+        doc.style.setProperty('—app-height', `${window.innerHeight}px`)
+    }
+    window.addEventListener('resize', appHeight)
+    appHeight()
+}
+
+heightScreenFixer()
+
+function openWhatsapp(input) {
+    const number = removeNoNumbers(input.value);
+    const url = `https://wa.me/${number}`;
+    window.open(url)
+}
+
+function inputValidator(inputElementRef, sendButtonElementRef,  errorMessageElementRef){
+    const number = removeNoNumbers(inputElementRef.value);
+    !!number?  sendButtonElementRef.removeAttribute('disabled'): sendButtonElementRef.setAttribute('disabled', '');
+    !!number? errorMessageElementRef.classList.remove('jao-ws__error-message--show') : errorMessageElementRef.classList.add('jao-ws__error-message--show')
+}
+
+
+function removeNoNumbers(text) {
+    return text.replace(/\D/g, '');
+}
+
+function paste(inputElementRef, sendButtonElementRef, errorMessageElementRef){
+    navigator.clipboard.readText().then((clipText) => {
+        inputElementRef.value = clipText
+        inputValidator(inputElementRef, sendButtonElementRef, errorMessageElementRef)
+    });
+}
+
 
 sendButtonWrapperElementRef.addEventListener('touchstart', () => {
     inputValidator(inputElementRef, sendButtonElementRef, errorMessageElementRef)
@@ -24,11 +57,11 @@ sendButtonElementRef.addEventListener('click', () => {
 
 inputElementRef.addEventListener('input', () => {
     inputValidator(inputElementRef, sendButtonElementRef, errorMessageElementRef)
-})
+}, { passive: true });
 
 pasteButtonElementRef.addEventListener('click', () => {
     paste(inputElementRef, sendButtonElementRef, errorMessageElementRef)
-})
+}, { passive: true });
 
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
